e4a2bc0d4f25dbe94c32f2453c2bcd45ea2b13b4
Keren WuRohe <kerenrohe@gmail.com>
2018-07-22 01:57:41 -0400

    Flixter fixed user enrollments issue

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,19 +2,16 @@ class LessonsController < ApplicationController
   before_action :authenticate_user!
   before_action :require_authorized_for_current_course, only: [:show]
   
-  def show
-  end
-  
   private
-  
-  def require_authorized_for_current_course
-    if ! current_user.enrolled_in?(current_lesson.section.course)
-      redirect_to course_path, alert: 'You must be logged in to view that page.'
-    end
-  end
   
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+  
+  def require_authorized_for_current_course
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: 'You must be enrolled in this course to view that page.'
+    end
   end
 end
