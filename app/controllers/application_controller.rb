d1727d065b75800796c45a8cfb1d62f66e8b7798
Keren WuRohe <kerenrohe@gmail.com>
2018-07-21 22:20:14 -0400

    Flixter Lesson 34 - dashboard setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # could write a method here to render response - unauthorized that can be called by controllers
 end
